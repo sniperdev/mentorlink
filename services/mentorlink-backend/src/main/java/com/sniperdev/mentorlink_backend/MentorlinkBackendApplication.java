6e63541b097648475fd778c4ff6cf652c0f8049c
sniperdev <igorspychalaa@gmail.com>
2026-07-07 20:26:42 +0200

    Initial commit

---
+++ services/mentorlink-backend/src/main/java/com/sniperdev/mentorlink_backend/MentorlinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sniperdev.mentorlink_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorlinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorlinkBackendApplication.class, args);
+	}
+
+}
---
+++ services/mentorlink-backend/src/test/java/com/sniperdev/mentorlink_backend/MentorlinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sniperdev.mentorlink_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorlinkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
